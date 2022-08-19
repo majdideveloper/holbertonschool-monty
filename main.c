2ddff5b52633849eba180f15e48883b9cf24308b
imenmansouri93 <imennmansourii@gmail.com>
2022-08-19 16:05:58 +0100

    main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ Return : success or failure
 int main(int argc, char **argv)
 {
 	FILE *monty_file = NULL;
-    size_t len  =  0;
+    size_t size  =  0;
     char *contents = NULL;
     int exit_status = EXIT_SUCCESS;
 	if (argc != 2)
@@ -19,7 +19,7 @@ monty_file = fopen(argv[1], "r");
 		exit(EXIT_FAILURE);
         }
 	exit_status  = run_monty(monty_file);
-    read = getline(&contents,&len,monty_file);
+    getline(&contents,&size,monty_file);
 	fclose(monty_file); 
 	return(exit_status);
 }
