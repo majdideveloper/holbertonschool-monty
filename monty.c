78242224a685660997ac93e11e7a0a9a36bf4bca
majdi aribi <majdi.developer@gmail.com>
2022-08-20 09:55:23 +0100

    try to fix push other time

--- monty.c
+++ monty.c
@@ -1,25 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-/**
-* main - function monty program
-Return : success or failure
-*/
-int main(int argc, char **argv)
-{
-	FILE *monty_file = NULL;
-	int exit_status = EXIT_SUCCESS;
+#include "monty.h"
 
-	if (argc != 2)
-		/* FUNCTION ERROR/
-		return;
-	monty_file = fopen(argv[1], "r"); /* open file/
-	
-	if (monty_file == NULL)
-		
-		return;
-	
-	exit_status = run_monty(monty_file); /**function that execute the monty bytecodes, check the file,the option need it like push pall swap..., I think that it will be like printf main.c and return exit status success or faill */ 
-	
-	fclose(monty_file); 
-	return(exit_status);
+glob_t global = {NULL, NULL};
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Arguments
+ * Return: number of arguments.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+		handle_command(argv[1]);
+	else
+	{
+		dprintf(STDERR_FILENO, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	return (0);
 }
--- monty.h
+++ monty.h
@@ -1,10 +1,15 @@
 #ifndef MONTY_H
 #define MONTY_H
-#include <iostream>
-#include <string.h>
-#include <stdlib.h>
+
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -12,31 +17,64 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-void push (struct stack_t** stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
-void add (struct stack_t** stack, unsigned int line_number);
-int checkval(char *value, unsigned int line_numer);
-void get_function(stack_t **stack, unsigne int line_number, char  *operation);
+
+/**
+ * struct glob_s - global and its funcs
+ * @fd: File descriptor
+ * @line: Line to getline
+ *
+ * Description: To handle the file and getline
+ */
+typedef struct glob_s
+{
+	FILE *fd;
+	char *line;
+} glob_t;
+
+extern glob_t global;
+extern int value;
+
+void handle_command(char *argv);
+
+int get_opc(stack_t **stack, char *arg, char *item, int count);
+
+void _push(stack_t **stack, unsigned int line_number);
+void _pall(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
+void cleanStack(stack_t **stack);
+
+/*Help*/
+int _isdigit(char *c);
+stack_t *new_Node(int n);
+
+/* handle_errors */
+void push_error(FILE *fd, char *line, stack_t *stack, int count);
+void ins_error(FILE *fd, char *line, stack_t *stack, char *count, int item);
+
 #endif
